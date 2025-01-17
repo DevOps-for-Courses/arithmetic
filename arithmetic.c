6f289e2512cba600ac3fdd7d87d3615cec16fbca
Joel Coffman <joel.coffman@afacademy.af.edu>
2025-01-17 15:39:12 -0700

    Add example implementation of solution

--- arithmetic.c
+++ arithmetic.c
@@ -2,26 +2,24 @@
  * Implementation of basic arithmetic operations in C.
  */
 
-#include <math.h>
-
 #include "arithmetic.h"
 
 // Add two numbers and return the sum
 double add(double x, double y) {
-  return INFINITY;
+  return x + y;
 }
 
 // Subtract two numbers and return the difference
 double subtract(double x, double y) {
-  return INFINITY;
+  return x - y;
 }
 
 // Multiply two numbers and return the product
 double multiply(double x, double y) {
-  return INFINITY;
+  return x * y;
 }
 
 // Divide two numbers and return the quotient
 double divide(double x, double y) {
-  return INFINITY;
+  return x / y;
 }
